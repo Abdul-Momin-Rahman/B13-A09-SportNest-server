d189db0febc7f20139864ea772bd69cdd316eac3
Abdul-Momin-Rahman <a.mominatick@gmail.com>
2026-05-21 22:39:20 +0600

    backend variables changed

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ const client = new MongoClient(uri, {
 });
 
 const JWKS = createRemoteJWKSet(
-    new URL("http://localhost:3000/api/auth/jwks")
+    new URL(`${process.env.CLIENT_URL}/api/auth/jwks`)
 )
 
 const verifyToken = async (req, res, next) => {
@@ -55,7 +55,7 @@ const verifyToken = async (req, res, next) => {
 async function run() {
     try {
 
-        await client.connect();
+        // await client.connect();
 
         const db = client.db("SportNest");
         const facilities = db.collection("Facilities");
@@ -164,7 +164,7 @@ async function run() {
             res.json(result)
         })
 
-        await client.db("admin").command({ ping: 1 });
+        // await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
     } finally {
 
